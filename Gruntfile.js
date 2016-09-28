8830f70ad72e535bef78f39878db633cdac412f1
luojinghui <939035243@qq.com>
2016-09-28 12:02:14 +0800

    init jnexpert-v4.0 project

---
+++ Gruntfile.js
@@ -0,0 +1,174 @@
+module.exports = function(grunt) {
+
+    grunt.event.on('qunit.spawn', function (url) {
+      grunt.log.ok('Running test: ' + url);
+    });
+
+  // 项目配置
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+            ' * version:<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under the <%= pkg.license %> license\n' +
+            ' */\n',
+    uglify: {
+        options: {
+            stripBanners: true,
+            //banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+            banner: '/*!\n' +
+            ' * version:<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under the <%= pkg.license %> license\n' +
+            ' */\n',
+            footer:'\n/*! <%= pkg.name %> 最后修改于： <%= grunt.template.today("yyyy-mm-dd") %> */',//添加footer
+            beautify: {
+              beautify: false
+            },
+            mangle: false,
+           sourceMap: true
+        //   mangle: true
+        },
+        sea: {
+            files: {
+                src: ['js/seajs-version/ldialog.js'],
+                dest: 'js/seajs-version/ldialog.min.js',
+                sourceMap: true
+            }
+        },
+        bulid2: {
+            src: ['js/ldialog.js'],
+            dest: 'js/ldialog.min.js',
+            sourceMap: true
+        }
+    },
+    jshint: {
+        files: ['Grungfile.js', 'public/*.js'],
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        ignores: {jshintignore: '.jshintignore'}
+    },
+    babel: {
+      options: {
+          sourceMap: false,
+          //presets: ['es2015'],
+          babelrc: true
+      },
+      dist: {
+          files: {
+              'js/demo2.js': 'js/demo.js'
+          },
+          extends: '.babelrc'
+      }
+    },
+    watch: {
+        // files: ['public/*.js'],
+        // tasks: ['jshint'],
+        options: {
+           livereload: true
+        },
+        less: {
+             files: ["./less/*"],
+             tasks: ["less", "autoprefixer:development"],
+             options: {
+               livereload: true
+           }
+        }
+        // jshint: {
+        //     files: 'public/*.js',
+        //     tasks: ['jshint']
+        // },
+        // babel: {
+        //     files: 'js/demo.js',
+        //     tasks: ['babel']
+        // }
+    },
+    concat:{
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */',
+        //定义一个字符串插入没个文件之间用于连接输出
+        separator: ';'
+      },
+      dist: {
+          src: ['public/question.js','public/Answer.js'],
+          dest: 'build/concat.js'
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ['./css'],
+          yuicompress: false
+        },
+        files: {
+          './css/form.css': './less/form.less'
+          // './css/test2.css': './less/style2.less'
+        }
+      }
+    },
+    cssmin: {
+        options: {
+          sourceMap: true,
+          compatibility: 'ie8',
+          keepSpecialComments: '*'
+        },
+        files: {
+            src: 'css/ldialog.css',
+            dest: 'css/ldialog.min.css'
+        }
+    },
+    autoprefixer: {
+        development: {
+                options: {
+                    browsers: ['last 5 versions', '> 1%' , 'Firefox 15'],
+                },
+                expand: true,
+                flatten: true,
+                src: './css/form.css',
+                dest: './css',
+                watch : {
+                         styles : {
+                              files : ['public/*.css' ],
+                              tasks : ['autoprefixer' ]
+                         }
+                    }
+              }
+    },
+    csscomb: {
+        options: {
+                config: 'public/css/sort.json'
+            },
+        main: {
+               files: {
+                   './public/css/aaa.css': './public/css/aa.css'
+               }
+           }
+    },
+    qunit: {
+      files: ['views/test.html']
+    }
+  });
+
+  // 加载Grunt插件
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-csscomb');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-babel');
+
+
+  // 注册grunt默认任务
+  grunt.registerTask('check', ['jshint']);
+  grunt.registerTask('default', 'My "default" task description.', function() {
+    //   grunt.task.run('uglify', 'jshint', 'less');
+      grunt.log.writeln('Currently running the "default" task.');
+    });
+};
